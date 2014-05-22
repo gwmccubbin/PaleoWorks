a2bbfb3f1e5db7849371c100edd619b7dda82f9b
Gregory McCubbin <gwmccubbin@gmail.com>
2014-05-22 16:25:44 -0500

    Configures flash notice responders for 'Repsonders' gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  self.responder = ApplicationResponder
   protect_from_forgery with: :exception
   respond_to :html
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  responders :flash
   def index
   end
 
