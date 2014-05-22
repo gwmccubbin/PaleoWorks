23bc0eff40f0059cbbd4d5c74d612323bea0f384
Gregory McCubbin <gwmccubbin@gmail.com>
2014-05-22 14:18:22 -0500

    Removes unused associations in Customer model. Associates Customer model with Address, AwayDate, StandardOrder, and Order models.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,11 @@
 class Customer < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
-  has_one :address
+  has_many :addresses
+  has_many :away_dates
+  has_many :standard_orders
+  has_many :orders
   has_many :customer_locations
   has_many :locations, through: :customer_locations
-  has_many :customer_away_times
-  has_many :away_times, through: :customer_away_times
-  has_many :customer_recurring_orders
-  has_many :recurring_orders, through: :customer_recurring_orders
 
   def full_name
     "#{first_name} #{last_name}"
