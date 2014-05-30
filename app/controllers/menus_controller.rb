c19213e849ab9f6730b475b64d06297ef084d395
Gregory McCubbin <gwmccubbin@gmail.com>
2014-05-30 11:23:22 -0500

    Sets up CRUD methods in Menus Controller.

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,34 @@
 class MenusController < ApplicationController
+  load_and_authorize_resource
+
   def index
   end
+
+  def show
+  end
+
+  def new
+    # [@breakfasts, @lunches, @dinners].each { |i| i  = Array.new(3) { @menu.menu_items.build } }
+    @breakfasts = Array.new(3) { @menu.menu_items.build }
+    
+  end
+
+  def create
+    binding.pry
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  # def menu_params
+  #   params.require(:menu).permit(:item)
+  # end
 end
