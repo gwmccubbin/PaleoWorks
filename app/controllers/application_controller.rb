ccddf09df146a28338a2b9e706175d388cbc9e98
Gregory McCubbin <gwmccubbin@gmail.com>
2014-05-31 12:33:31 -0500

    Adds before_action authenticater_user to Application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   self.responder = ApplicationResponder
   protect_from_forgery with: :exception
   respond_to :html
