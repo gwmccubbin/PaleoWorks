8fea46eb24b29d5eb1c385c0d1d56ac4af108821
Sean Hill <seandhi@gmail.com>
2014-05-31 16:14:04 -0500

    Add geocoding to addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,19 @@ class Address < ActiveRecord::Base
   belongs_to :location
   validates_presence_of :address1, :zip
 
+  geocoded_by :full
+
+  reverse_geocoded_by :latitude, :longitude do |obj, results|
+    if geo = results.first
+      obj.address1 = geo.street_address
+      obj.city = geo.city
+      obj.state = geo.state
+    end
+  end
+
+  before_validation :geocode
+  before_validation :reverse_geocode
+
   def street
     "#{address1} #{address2}"    
   end
