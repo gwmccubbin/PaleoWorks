fe34ccaf710a801b1f352efc65b9552531c1ab8b
Sean Hill <seandhi@gmail.com>
2014-05-31 18:00:05 -0500

    Refactor standard order display to helper

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,14 @@
 module CustomersHelper
+  def display_standard_order
+    result = ""
+    [:breakfast, :lunch, :dinner, :special].each do |type|
+      method = "#{type}_qty".to_sym
+      unless @standard_order.send(method).nil?
+        result += "<dt>#{type.to_s.capitalize.pluralize}:</dt>"
+        result += "<dd>#{@standard_order.send(method)}</dd>"
+      end
+    end
+
+    result.html_safe
+  end
 end
