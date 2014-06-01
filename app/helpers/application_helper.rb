b7fa780081980e52e2277bacbe2914bf0fc41d4e
Sean Hill <seandhi@gmail.com>
2014-05-31 22:21:37 -0500

    Create helpers for building form icon inputs and default panels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,20 @@ module ApplicationHelper
     end
   end
 
+  def default_panel(title, &block)
+    content_tag :div, class: 'panel panel-default' do
+      content = content_tag :div, class: 'panel-heading' do
+        content_tag :h3, title
+      end
+
+      content += content_tag(:div, class: 'panel-body') do
+        capture(&block)
+      end
+
+      content
+    end
+  end
+
   def delivery_type(custom_delivery)
     if custom_delivery == true
       "Custom Delivery"
@@ -25,5 +39,26 @@ module ApplicationHelper
 
   def human_boolean(boolean)
     boolean ? 'Yes' : 'No'
+  end
+
+  def icon_input(form_object, field, klass, placeholder, options = {})
+    options = options.merge(placeholder: placeholder, addon_html: { class: klass })
+    form_object.input field, options
+  end
+
+  def cutlery_icon_input(form_object, field, placeholder, options = {})
+    icon_input form_object, field, 'glyphicon glyphicon-cutlery', placeholder, options
+  end
+
+  def email_icon_input(form_object, field, placeholder, options = {})
+    icon_input form_object, field, 'icon-email', placeholder, options
+  end
+
+  def home_icon_input(form_object, field, placeholder, options = {})
+    icon_input form_object, field, 'glyphicon glyphicon-home', placeholder, options
+  end
+
+  def phone_icon_input(form_object, field, placeholder, options = {})
+    icon_input form_object, field, 'glyphicon glyphicon-phone-alt', placeholder, options
   end
 end
