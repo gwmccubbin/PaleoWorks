043a08f5ba093b0fb143b478d6fb134fe4bfb441
Gregory McCubbin <gwmccubbin@gmail.com>
2014-06-09 15:56:26 -0500

    Runs migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531205918) do
+ActiveRecord::Schema.define(version: 20140609174053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20140531205918) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "order_id"
   end
 
   create_table "orders", force: true do |t|
