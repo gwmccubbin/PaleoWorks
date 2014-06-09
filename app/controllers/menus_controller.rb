3d662dd0dfc4bc42e518d3250fcf16730975c5b8
Gregory McCubbin <gwmccubbin@gmail.com>
2014-06-09 15:48:55 -0500

    Makes live all CRUD actions in Menus controller.

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,34 +1,54 @@
 class MenusController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource except: [:index]
 
   def index
+    @menus = Menu.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
   end
 
   def new
-    # [@breakfasts, @lunches, @dinners].each { |i| i  = Array.new(3) { @menu.menu_items.build } }
-    @breakfasts = Array.new(3) { @menu.menu_items.build }
-    
   end
 
   def create
-    binding.pry
+    @menu = Menu.new menu_params
+
+    path = if params[:commit] == 'Save Menu'
+             menus_path
+           elsif params[:commit] == 'Save & Add New'
+             new_menu_path
+           end
+
+    @menu.save
+    respond_with @menu, location: path
   end
 
   def edit
   end
 
   def update
+    @menu.update_attributes menu_params
+
+    path = if params[:commit] == 'Update Menu'
+             menus_path
+           elsif params[:commit] == 'Save & Continue'
+             edit_menu_path @menu
+           end
+
+    @menu.save
+    respond_with @menu, location: path
   end
 
   def destroy
+    @menu.destroy
+
+    respond_with @menu, location: [:menus]
   end
 
   private
 
-  # def menu_params
-  #   params.require(:menu).permit(:item)
-  # end
+  def menu_params
+    params.require(:menu).permit(:name, item_ids: [])
+  end
 end
