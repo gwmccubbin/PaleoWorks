fd2258744f279470b33e7913aacf3132991a74d6
Gregory McCubbin <gwmccubbin@gmail.com>
2014-06-09 15:43:30 -0500

    Adds :dependent => :destroy to menu_items in Menu model.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,6 @@
 class Menu < ActiveRecord::Base
   has_many :menu_items
+  has_many :menu_items, dependent: :destroy
   has_many :items, through: :menu_items
   accepts_nested_attributes_for :menu_items
 end
