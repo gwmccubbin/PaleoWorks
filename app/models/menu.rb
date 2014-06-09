db524efdc6702549c1a2d5bff3f7862352d4b94e
Gregory McCubbin <gwmccubbin@gmail.com>
2014-06-09 15:47:14 -0500

    Adds validation for name in Menu model. Adds 'current' scope in menu model. Adds 'current?' method in menu model.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,12 @@
 class Menu < ActiveRecord::Base
-  has_many :menu_items
   has_many :menu_items, dependent: :destroy
   has_many :items, through: :menu_items
   accepts_nested_attributes_for :menu_items
+  validates :name, presence: true
+
+  scope :current, -> { where('start_date < ?', Time.now).where('end_date > ?', Time.now) }
+
+  def current?
+    Menu.current.include?(self)
+  end
 end
