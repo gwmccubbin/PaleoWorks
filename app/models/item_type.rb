b91e806e56ea7a9a2a104fc18acda57fc78780d4
Gregory McCubbin <gwmccubbin@gmail.com>
2014-06-09 15:51:50 -0500

    Adds methods to get item type ids in Item Type model.

--- app/models/item_type.rb
+++ app/models/item_type.rb
@@ -1,3 +1,22 @@
 class ItemType < ActiveRecord::Base
   has_many :items
+
+  class << self
+
+    def breakfast_id
+      where('name LIKE ?', 'Breakfast').first.id  
+    end
+    
+    def lunch_id
+      where('name LIKE ?', 'Lunch').first.id  
+    end
+    
+    def dinner_id
+      where('name LIKE ?', 'Dinner').first.id  
+    end
+    
+    def special_id
+      where('name LIKE ?', 'Special').first.id  
+    end
+  end
 end
