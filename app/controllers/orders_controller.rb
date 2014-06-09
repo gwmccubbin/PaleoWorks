1a4193d6affe944c918cb0b74b0f9e93708c5bb5
Gregory McCubbin <gwmccubbin@gmail.com>
2014-06-09 15:58:15 -0500

    Adds Orders controller.

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,27 @@
+class OrdersController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def order_params
+    
+  end
+end
