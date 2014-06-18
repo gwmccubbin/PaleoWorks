d29439fa2df864467024e61118fa9498158f2dfa
Sean Hill <seandhi@gmail.com>
2014-06-18 10:38:42 -0500

    Accept nested attributes for order_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ActiveRecord::Base
   belongs_to :location
   has_many :order_items
   has_many :items, through: :order_items
+
+  accepts_nested_attributes_for :order_items, allow_destroy: true
 end
