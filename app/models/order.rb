42a7e114b5b874bcae08d859d0d57c18d726ce4e
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 11:09:08 -0500

    Delegates delivery_address and delivery_location_name to customer in order model; adds validations to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,17 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
   belongs_to :location
-  has_many :order_items
-  has_many :items, through: :order_items
+  has_many :order_items, dependent: :destroy
+
+  validates :customer, presence: true
+  validates :delivery_date, presence: true
 
   after_save :set_location_id
 
   accepts_nested_attributes_for :order_items, allow_destroy: true
-end
+
+  delegate :delivery_address, :delivery_location_name, to: :customer, prefix: true, allow_nil: true
+
 
   private
 
