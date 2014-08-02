06489c6c1cf5addc72949b27c785ccd328033a53
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 13:36:46 -0500

    Adds validations for start date and end date in menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,6 +3,9 @@ class Menu < ActiveRecord::Base
   has_many :items, through: :menu_items
   accepts_nested_attributes_for :menu_items
   validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
 
   scope :current, -> { where('start_date <= :date AND end_date >= :date', date: Date.today) }
 
