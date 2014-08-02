823c687f4272c536c3fc40f18409db534290ebae
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 11:08:02 -0500

    Adds callback in order model to set delivery location id based upon customer location

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,18 @@ class Order < ActiveRecord::Base
   has_many :order_items
   has_many :items, through: :order_items
 
+  after_save :set_location_id
+
   accepts_nested_attributes_for :order_items, allow_destroy: true
+end
+
+  private
+
+  def set_location_id
+    if location_id.nil?
+      if !customer.delivery && customer.location.present?
+        update(location_id: customer.location.id)   
+      end
+    end
+  end
 end
