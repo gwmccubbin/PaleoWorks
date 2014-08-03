09ae75975b88ffa7efa9460d3bd9ff6228324fca
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 20:43:25 -0500

    Adds methods to retrieve items by meal type in menu model; formats code in menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,15 +1,32 @@
 class Menu < ActiveRecord::Base
   has_many :menu_items, dependent: :destroy
   has_many :items, through: :menu_items
+
   accepts_nested_attributes_for :menu_items
+
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-
 
   scope :current, -> { where('start_date <= :date AND end_date >= :date', date: Date.today) }
 
   def current?
     start_date <= Date.today and end_date >= Date.today
+  end
+
+  def breakfasts
+    items.where(item_type_id: ItemType.breakfast_id)
+  end
+
+  def lunches
+    items.where(item_type_id: ItemType.lunch_id)
+  end
+
+  def dinners
+    items.where(item_type_id: ItemType.dinner_id)
+  end
+
+  def specials
+    items.where(item_type_id: ItemType.special_id)    
   end
 end
