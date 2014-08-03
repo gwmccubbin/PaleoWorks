60af9ed2470998eb7910e8e2ef47d1b08a4f1199
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 20:40:41 -0500

    Removes association to orders in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :item_type
   has_many :order_items
-  has_many :orders, through: :order_items
   has_many :menu_items
   has_many :menus, through: :menu_items
   validates_numericality_of :cost, :price
