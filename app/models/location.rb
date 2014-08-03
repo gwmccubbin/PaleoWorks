718066ae873b6eed384e9f710d38bdde29712a7c
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 20:42:48 -0500

    Adds 'top' scope to location model; formats code in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,8 +2,17 @@ class Location < ActiveRecord::Base
   has_one :address
   has_many :customers
   has_many :orders
+
   accepts_nested_attributes_for :address
+  
   validates :name, presence: true
+
+  scope :top, ->(n) { 
+                      joins(:orders)
+                      .group("locations.id, orders.location_id")
+                      .order('count(orders.location_id) DESC')
+                      .limit(n) 
+                    }
 
   def street_address
     if address.address2
