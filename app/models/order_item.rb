59854400717477174c287693bef80e2b93f05000
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 22:19:03 -0500

    Adds dashboard summary query to order item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,15 @@ class OrderItem < ActiveRecord::Base
   belongs_to :item
 
   delegate :id, :name, :price, to: :item, prefix: true
+
+  class << self
+    def dashboard_summary
+      select('items.name, item_types.name as item_type_name, sum(order_items.qty) as total_quantity')
+      .joins(:order)
+      .joins(:item)
+      .joins(item: :item_type)
+      .group('items.id, item_types.name')
+      .merge(Order.current).group_by { |oi| oi.item_type_name }
+    end
+  end
 end
