c2db9b3aff5db5c6e752aa5e57e32234e02c1979
Gregory McCubbin <gwmccubbin@gmail.com>
2014-08-02 20:48:25 -0500

    Adds code to wire up dashboard in dashboard controller; retrieves top_locations, recent customers, and current_menu; adds count helper methods for active customers, inactive customers, customers, locations, custom delivery orders, gym delivery orders

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,42 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :instantiate_customer_and_build_relationships, only: [:show]
+  helper_method :active_customer_count, :inactive_customer_count, :customer_count
+  helper_method :location_count, :custom_delivery_order_count, :gym_delivery_order_count
+
   def show
+    @top_locations = Location.top(5)
+    @recent_customers = Customer.recent(5)
+    @current_menu = Menu.current.includes(:items).first
+  end
+
+  private
+
+  [:inactive_customer_count, :active_customer_count, :customer_count].each do |type|
+    define_method type do      
+      unless instance_variable_defined? "@type"
+        instance_variable_set "@#{type.to_s}", Customer.send(type.to_s.gsub('customer_', ''))
+      end
+
+      instance_variable_get "@#{type.to_s}"
+    end
+  end
+
+  def location_count
+    @location_count ||= Location.count
+  end
+
+  def gym_delivery_order_count
+    @gym_delivery_order_count ||= Order.gym_delivery.count
+  end
+
+  def custom_delivery_order_count
+    @custom_delivery_order_count ||= Order.custom_delivery.count
+  end
+
+  def instantiate_customer_and_build_relationships
     @customer = Customer.new
     @customer.addresses.build
     @customer.build_standard_order
   end
-
 end
